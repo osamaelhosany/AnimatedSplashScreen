83963e5002dfc2df41e6dd716e73528ec9adf2de
Osama ELhussiny <osama.elhosany@yahoo.com>
2020-09-30 02:57:43 +0200

    added Android Splash Screen

--- AnimatedSplashScreen.Android/MainActivity.cs
+++ AnimatedSplashScreen.Android/MainActivity.cs
@@ -9,7 +9,11 @@ using Android.OS;
 
 namespace AnimatedSplashScreen.Droid
 {
-    [Activity(Label = "AnimatedSplashScreen", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "AnimatedSplashScreen",
+        Icon = "@mipmap/icon",
+        Theme = "@style/MainTheme",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -21,6 +25,8 @@ namespace AnimatedSplashScreen.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Lottie.Forms.Droid.AnimationViewRenderer.Init();
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
---
+++ AnimatedSplashScreen.Android/SplashActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.Animation;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Com.Airbnb.Lottie;
+using System.Threading.Tasks;
+
+namespace AnimatedSplashScreen.Droid
+{
+    [Activity(Label = "Sample",
+        Icon = "@drawable/icon",
+        Theme = "@style/Theme.Splash",
+        MainLauncher = true,
+        NoHistory = true)]
+    public class SplashActivity : Activity, Animator.IAnimatorListener
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Activity_Splash);
+
+            var animationView = FindViewById<LottieAnimationView>(Resource.Id.animation_view);
+            animationView.AddAnimatorListener(this);
+            //Task.Delay(500);
+            //Finish();
+            //StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        public void OnAnimationCancel(Animator animation)
+        {
+        }
+
+        public void OnAnimationEnd(Animator animation)
+        {
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        public void OnAnimationRepeat(Animator animation)
+        {
+        }
+
+        public void OnAnimationStart(Animator animation)
+        {
+        }
+    }
+}
